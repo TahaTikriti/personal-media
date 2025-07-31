d47d4e36acf7687b986ca17b869709e4f1be6b43
TahaTikriti <126393773+TahaTikriti@users.noreply.github.com>
2025-07-31 13:46:52 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: "./", // add this line
   plugins: [react()],
+  base: "./", // 👈 Add this line
 })
